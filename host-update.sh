8991e957075247c8990d9865a2897369639a8e53
jonc-techdata <jonc-techdata@outlook.com>
2017-08-16 12:24:53 +1000

    update host-update.sh for hostname change

--- host-update.sh
+++ host-update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo && echo
-read -p "Please enter host node number: " new
+echo && read -p "Please enter host node number: " new
 
 if ! [ $new -eq $new ] 2>/dev/null ; then
         echo -e "$(tput setaf 1)!! Exit -- Sorry, integer only !!$(tput sgr0)"
@@ -19,15 +18,18 @@ oldip=$(cat /etc/network/interfaces | grep "iface enp0s3" -A 2 \
 newhost=$(echo $oldhost | cut -d- -f1)-$new
 newip=$(echo $oldip | cut -d. -f4 --complement).$new
 
-sudo sed -i "s/$oldhost/$newhost/" /etc/hostname
-sudo sed -i "s/$oldhost/$newhost/" /etc/hosts
-sudo sed -i "s/$oldip/$newip/" /etc/network/interfaces
+read -p "Change hostname? [enter] for no change: " new
+if [ ! -z $new ] ; then  newhost=$new-$(echo $newhost | cut -d- -f2) ; fi
 
 echo
 echo "$(tput setaf 6)!! Update node name from $oldhost to $newhost !!"
 echo "!! Update node name from $oldip to $newip !! $(tput sgr0)"
-echo System will restart in 10 seconds
+echo && echo System will restart in 10 seconds
 sleep 10
+
+sudo sed -i "s/$oldhost/$newhost/" /etc/hostname
+sudo sed -i "s/$oldhost/$newhost/" /etc/hosts
+sudo sed -i "s/$oldip/$newip/" /etc/network/interfaces
 
 echo Restarting ........
 sudo shutdown -r now
