bbfadf9c05925d1203490ce09f32f20072cdc7b6
jonc-techdata <jonc-techdata@outlook.com>
2017-08-22 18:03:11 +1000

    update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo
+if [ "${USER}" = "root" ] ; then
+   echo -e "$(tput setaf 1)!! Do not use sudo for installation$(tput sgr0)"
+   exit 0 ; fi
+duser="${USER}" 
+
 # Add GPG key for the official Docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -18,15 +24,16 @@ echo "!! Docker installation will start in 10 seconds !! $(tput sgr0)"
 sleep 10s
 echo && sudo apt-get install -y docker-ce
 
-# Add user to Docker group
-sudo usermod -aG docker ${USER}
+# Add current user to Docker group
+# sudo usermod -aG docker $duser
+sudo gpasswd -a $duser docker
 
 echo && echo "$(tput setaf 6)!! Docker installation finished. !!$(tput sgr0)"
 echo && echo Type below command to check docker status:
 echo "$(tput setaf 6)sudo systemctl status docker$(tput sgr0)"
 echo && echo Type below commands to check user group:
-echo "$(tput setaf 6)su - ${USER}"
+echo "$(tput setaf 6)su - $duser"
 echo "id -nG"
 echo "exit$(tput sgr0)"
-echo "## return to non-root user.."
+echo "## re-login to activate membership in docker group ##"
 echo 
